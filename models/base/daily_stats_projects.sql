5b4d82e67c5cb20ef02ccb9ed7e4c50137956cce
Aaron Phethean <aphethean@matatika.com>
2020-12-09 21:54:04 +0000

    Added daily project stats transforms for aged W/O

--- models/base/daily_stats_projects.sql
+++ models/base/daily_stats_projects.sql
@@ -8,8 +8,22 @@ with stats as (
         report_day,
 
         -- aggregations
-	    sum(total_projects) as total_projects,
+        sum(total_projects) as total_projects,
         sum(total_workitems) as total_workitems,
+		sum(total_open) as total_open,
+        sum(sum(total_open)) 
+            over (order by report_year, report_month, report_day rows between 6 preceding and current row)
+            as total_open_last_7days,
+        sum(sum(total_open)) 
+            over (order by report_year, report_month, report_day rows between 13 preceding and current row)
+            as total_open_last_14days,
+        sum(sum(total_open)) over (
+                ORDER BY report_year, report_month, report_day ROWS BETWEEN UNBOUNDED PRECEDING AND 7 PRECEDING
+            ) as total_older_than_7days,
+        sum(sum(total_open)) over (
+                ORDER BY report_year, report_month, report_day ROWS BETWEEN UNBOUNDED PRECEDING AND 14 PRECEDING
+            ) as total_older_than_14days,
+        sum(total_closed) as total_closed,
         round(avg(first_response_hours)::numeric, 1) as avg_first_response_hours,
         sum(response_within_sla) as total_response_within_sla,
         round(avg(final_fix_hours)::numeric, 1) as avg_final_fix_hours,
@@ -36,6 +50,12 @@ daily_projects_stats as (
     
         total_projects,
         total_workitems,
+        total_open,
+        total_closed,
+        total_open_last_7days,
+        total_open_last_14days,
+        total_older_than_7days,
+        total_older_than_14days,
         avg_first_response_hours,
         total_response_within_sla,
         avg_final_fix_hours,
