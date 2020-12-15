7484fbb646d81ece61e2518b90aeb50f2afd270e
Aaron Phethean <aphethean@matatika.com>
2020-12-15 10:50:42 +0000

    Added a basic test for total_workitems

---
+++ tests/assert_project_sla_total_workitems.sql
@@ -0,0 +1,10 @@
+-- Given a project with multiple workitems (71602551 has 3 work items)
+-- When query vw_project_sla
+-- Expect total_workitems to be equal to the number of work project work items (consider using a count fact_workitem?)
+select
+    project_id,
+    sum(total_workitems)
+from {{ ref('vw_project_sla' )}}
+where project_id = '71602551'
+group by project_id
+having not(sum(total_workitems) = 3)
