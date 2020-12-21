0b8d06ec8f651c6ed336a456834b02ae8ef67758
Aaron Phethean <aphethean@matatika.com>
2020-12-21 14:37:35 +0000

    Remove redundant view - workitem generic sla

--- tests/assert_fact_workitem_remote_closed_following_assign_unassigned.sql
+++ tests/assert_fact_workitem_remote_closed_following_assign_unassigned.sql
@@ -1,16 +1,16 @@
 -- Given a project with multiple workitems (74125570 has 2 work items)
 -- Given one of those workitems has multiple assign / unassigned (a27fc53a-eca4-45d8-9f56-6155cdbcdf83)
--- When query vw_workitem_generic_sla
+-- When query fact_workitem
 -- Expect rows to be equal to 2
 
---select
---    project_id,
---    count(work_item_id)
---from {{ ref('vw_workitem_generic_sla' )}}
---where project_id = '74125570'
---group by project_id
---having not(count(work_item_id) = 2)
---union
+select
+    project_id,
+    count(work_item_id)
+from {{ ref('fact_workitem' )}}
+where project_id = '74125570'
+group by project_id
+having not(count(work_item_id) = 2)
+union
 select
     project_id,
     sum(is_closed) as total_closed
