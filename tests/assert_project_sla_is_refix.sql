0837ef0990dfcc6620636d042ffd42d0f7db37e0
Aaron Phethean <aphethean@matatika.com>
2021-01-15 10:53:20 +0000

    Add test for is_refix

---
+++ tests/assert_project_sla_is_refix.sql
@@ -0,0 +1,17 @@
+-- Given a project with a 'Reactivation' tagged work item
+-- When query vw_project_sla
+-- Expect reactivation_timestamp
+-- Expect is_refix to be equal to 1 (returning results is a test failure)
+select
+    project_id,
+    response_within_sla
+from {{ ref('vw_project_sla' )}}
+where project_id = '74864898'
+and is_refix = 0
+union
+select
+    project_id,
+    is_refix
+from {{ ref('vw_project_sla' )}} vp
+where reactivated_timestamp is not null
+and is_refix != 1
