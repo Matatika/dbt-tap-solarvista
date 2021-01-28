d964526510a7b3521ac64b74052577b4e8c6f8e5
Aaron Phethean <aphethean@matatika.com>
2021-01-28 16:44:45 +0000

    Restored schema to snapshot view

---
+++ snapshots/dim_project_snapshot.sql
@@ -0,0 +1,15 @@
+{% snapshot dim_project_snapshot %}
+
+{{
+    config(
+      target_database='jvdsuiab',	
+      target_schema='cityfm',
+      unique_key='reference',
+      strategy='check',      
+      check_cols=['status', 'createdon','closedon'],
+    )
+}}
+
+select * from "{{var('schema')}}".project_stream 
+
+{% endsnapshot %}
