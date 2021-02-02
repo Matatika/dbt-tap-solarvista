3a5702e385252fc0e4f8349e2d94c97161602c4a
Aaron Phethean <aphethean@matatika.com>
2021-02-02 21:52:48 +0000

    Upgrade the project configuration to version 2.  Remove a redundant test that used a reference to schema variable that could not resolve

--- tests/assert_project_stream_count_eq_dim_project_count.sql
+++
@@ -1,8 +0,0 @@
--- Given a number of projects in project_stream
--- When count dim_project
--- Expect count to be equal
-select
-    reference
-from "{{var('schema')}}".project_stream
-group by 1
-where reference not in (select reference from ref('dim_project' ))
