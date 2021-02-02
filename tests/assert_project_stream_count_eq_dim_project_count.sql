94208d90f1dbf247f7eb01bef6b853219c4b4c3a
Aaron Phethean <aphethean@matatika.com>
2021-02-02 21:12:48 +0000

    Fix test warning with reference to project_stream

--- tests/assert_project_stream_count_eq_dim_project_count.sql
+++ tests/assert_project_stream_count_eq_dim_project_count.sql
@@ -3,6 +3,6 @@
 -- Expect count to be equal
 select
     reference
-from {{ ref('project_stream' ) }}
+from "{{var('schema')}}".project_stream
 group by 1
 where reference not in (select reference from ref('dim_project' ))
