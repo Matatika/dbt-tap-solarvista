56e4a05cbd1518aa51fbdc44949f57a04ecd05ab
Sowmyalakshmi Sambandan <sowmya.sam@gmail.com>
2021-02-08 19:04:09 +0530

    Updated with changes for project napshots in fact and views
    New fact table and views added -
    1. fact_workitem_incremental
    2. vw_project_snapshot_sla
    3. vw_workitem_snapshot_stages

---
+++ models/base/vw_project_snapshot_sla.sql
@@ -0,0 +1,61 @@
+with workitems as (
+    select * from {{ ref('fact_workitem_incremental') }}
+),
+projects as (
+     select distinct * from {{ ref('dim_project_snapshot') }} 
+     where dbt_valid_to is null
+),
+project_workitem_count as (
+    select distinct
+        projects.reference as project_id,
+        workitems.count as total_workitems
+    from workitems
+    left join projects 
+        on projects.dbt_scd_id = workitems.projects_sk   
+    group by projects.reference
+),
+project_workitem_active as (
+    select distinct
+        projects.reference as project_id,
+        workitems.count as active_workitems
+    from workitems, projects 
+    where projects.dbt_scd_id = workitems.projects_sk 
+    and workitems.current_stage Not in ('Discarded','Closed','RemoteClosed','Rejected','Cancelled')
+    group by projects.reference
+),
+dates as (
+    select * from {{ ref('dim_date') }}
+),
+
+final as (
+    select
+        projects.reference as project_id,
+        createdon::date as report_date,
+        EXTRACT(YEAR FROM createdon)::integer as report_year,
+        EXTRACT(MONTH FROM createdon)::integer as report_month,
+        EXTRACT(DAY FROM createdon)::integer as report_day,
+    
+	    project_type, 
+	    status,  
+        createdon,
+        closedon,
+        appliedresponsesla,
+        responseduedate,
+        fixduedate,
+        total_workitems,
+        active_workitems,
+
+        dates.day_of_month,
+        dates.day_of_year,
+        dates.day_of_week,
+        dates.day_of_week_name,
+        dates.week_key,
+        dates.week_of_year
+
+    from projects
+        left outer join dates on dates.date_day = projects.createdon
+        left outer join project_workitem_count on project_workitem_count.project_id = projects.reference
+        left outer join project_workitem_active on project_workitem_active.project_id = projects.reference
+
+)
+select * from final
---
+++ models/base/vw_workitem_snapshot_stages.sql
@@ -0,0 +1,191 @@
+
+with workitems_hist as (
+    select * from {{ ref('fact_workitem_history') }}
+),
+workitems as (
+    select * from {{ ref('fact_workitem_incremental') }}
+),
+projects as (
+     select distinct * from {{ ref('dim_project_snapshot') }}
+     where dbt_valid_to is null
+),
+workitems_accepted as (
+--Sql to retrieve the earliest stage transition date
+select ht.work_item_id,ht.received_at
+FROM (
+    Select ht.work_item_id, Min(ht.stage_transition_received_at) AS MinDate
+    from workitems_hist ht 
+    where ht.stage_stage_type = 'Accepted' 
+    GROUP BY ht.work_item_id
+) AS t2
+INNER JOIN workitems_hist ht ON ht.work_item_id = t2.work_item_id AND ht.received_at= t2.MinDate
+),
+workitems_closed as (
+--Sql to retrieve the earliest stage transition date
+select ht.work_item_id,ht.received_at
+FROM (
+    Select ht.work_item_id, Min(ht.stage_transition_received_at) AS MinDate
+    from workitems_hist ht 
+    where ht.stage_stage_type = 'Closed' 
+    GROUP BY ht.work_item_id
+) AS t2
+INNER JOIN workitems_hist ht ON ht.work_item_id = t2.work_item_id AND ht.received_at= t2.MinDate
+),
+workitems_assigned as (
+--Sql to retrieve the earliest stage transition date
+select ht.work_item_id,ht.received_at
+FROM (
+    Select ht.work_item_id, Min(ht.stage_transition_received_at) AS MinDate
+    from workitems_hist ht 
+    where ht.stage_stage_type = 'Assigned' 
+    GROUP BY ht.work_item_id
+) AS t2
+INNER JOIN workitems_hist ht ON ht.work_item_id = t2.work_item_id AND ht.received_at= t2.MinDate
+),
+workitems_cancelled as (
+--Sql to retrieve the earliest stage transition date
+select ht.work_item_id,ht.received_at
+FROM (
+    Select ht.work_item_id, Min(ht.stage_transition_received_at) AS MinDate
+    from workitems_hist ht 
+    where ht.stage_stage_type = 'Cancelled' 
+    GROUP BY ht.work_item_id
+) AS t2
+INNER JOIN workitems_hist ht ON ht.work_item_id = t2.work_item_id AND ht.received_at= t2.MinDate
+),
+workitems_discarded as (
+--Sql to retrieve the earliest stage transition date
+select ht.work_item_id,ht.received_at
+FROM (
+    Select ht.work_item_id, Min(ht.stage_transition_received_at) AS MinDate
+    from workitems_hist ht 
+    where ht.stage_stage_type = 'Discarded' 
+    GROUP BY ht.work_item_id
+) AS t2
+INNER JOIN workitems_hist ht ON ht.work_item_id = t2.work_item_id AND ht.received_at= t2.MinDate
+),
+workitems_postworking as (
+--Sql to retrieve the earliest stage transition date
+select ht.work_item_id,ht.received_at
+FROM (
+    Select ht.work_item_id, Min(ht.stage_transition_received_at) AS MinDate
+    from workitems_hist ht 
+    where ht.stage_stage_type = 'PostWorking' 
+    GROUP BY ht.work_item_id
+) AS t2
+INNER JOIN workitems_hist ht ON ht.work_item_id = t2.work_item_id AND ht.received_at= t2.MinDate
+),
+workitems_preworking as (
+--Sql to retrieve the earliest stage transition date
+select ht.work_item_id,ht.received_at
+FROM (
+    Select ht.work_item_id, Min(ht.stage_transition_received_at) AS MinDate
+    from workitems_hist ht 
+    where ht.stage_stage_type = 'PreWorking' 
+    GROUP BY ht.work_item_id
+) AS t2
+INNER JOIN workitems_hist ht ON ht.work_item_id = t2.work_item_id AND ht.received_at= t2.MinDate
+),
+workitems_quickclose as (
+--Sql to retrieve the earliest stage transition date
+select ht.work_item_id,ht.received_at
+FROM (
+    Select ht.work_item_id, Min(ht.stage_transition_received_at) AS MinDate
+    from workitems_hist ht 
+    where ht.stage_stage_type = 'QuickClose' 
+    GROUP BY ht.work_item_id
+) AS t2
+INNER JOIN workitems_hist ht ON ht.work_item_id = t2.work_item_id AND ht.received_at= t2.MinDate
+),
+workitems_remoteclosed as (
+--Sql to retrieve the earliest stage transition date
+select ht.work_item_id,ht.received_at
+FROM (
+    Select ht.work_item_id, Min(ht.stage_transition_received_at) AS MinDate
+    from workitems_hist ht 
+    where ht.stage_stage_type = 'RemoteClosed' 
+    GROUP BY ht.work_item_id
+) AS t2
+INNER JOIN workitems_hist ht ON ht.work_item_id = t2.work_item_id AND ht.received_at= t2.MinDate
+),
+workitems_travellingfrom as (
+--Sql to retrieve the earliest stage transition date
+select ht.work_item_id,ht.received_at
+FROM (
+    Select ht.work_item_id, Min(ht.stage_transition_received_at) AS MinDate
+    from workitems_hist ht 
+    where ht.stage_stage_type = 'TravellingFrom' 
+    GROUP BY ht.work_item_id
+) AS t2
+INNER JOIN workitems_hist ht ON ht.work_item_id = t2.work_item_id AND ht.received_at= t2.MinDate
+),
+workitems_travellingto as (
+--Sql to retrieve the earliest stage transition date
+select ht.work_item_id,ht.received_at
+FROM (
+    Select ht.work_item_id, Min(ht.stage_transition_received_at) AS MinDate
+    from workitems_hist ht 
+    where ht.stage_stage_type = 'TravellingTo' 
+    GROUP BY ht.work_item_id
+) AS t2
+INNER JOIN workitems_hist ht ON ht.work_item_id = t2.work_item_id AND ht.received_at= t2.MinDate
+),
+workitems_unassigned as (
+--Sql to retrieve the earliest stage transition date
+select ht.work_item_id,ht.received_at
+FROM (
+    Select ht.work_item_id, Min(ht.stage_transition_received_at) AS MinDate
+    from workitems_hist ht 
+    where ht.stage_stage_type = 'Unassigned' 
+    GROUP BY ht.work_item_id
+) AS t2
+INNER JOIN workitems_hist ht ON ht.work_item_id = t2.work_item_id AND ht.received_at= t2.MinDate
+),
+workitems_working as (
+--Sql to retrieve the earliest stage transition date
+select ht.work_item_id,ht.received_at
+FROM (
+    Select ht.work_item_id, Min(ht.stage_transition_received_at) AS MinDate
+    from workitems_hist ht 
+    where ht.stage_stage_type = 'Working' 
+    GROUP BY ht.work_item_id
+) AS t2
+INNER JOIN workitems_hist ht ON ht.work_item_id = t2.work_item_id AND ht.received_at= t2.MinDate
+),
+vw_workitem_stages as (
+    select distinct 
+    workitems.work_item_id,
+    projects.reference,
+    projects.createdon,    
+    projects.responseduedate,
+    projects.fixduedate,
+    workitems_accepted.received_at as accepted,
+    workitems_closed.received_at as closed,  
+    workitems_assigned.received_at as assigned,  
+    workitems_cancelled.received_at as cancelled,  
+    workitems_discarded.received_at as discarded,  
+    workitems_postworking.received_at as postworking,  
+    workitems_preworking.received_at as preworking,  
+    workitems_quickclose.received_at as quickclose,  
+    workitems_remoteclosed.received_at as remoteclosed,  
+    workitems_travellingfrom.received_at as travellingfrom,  
+    workitems_travellingto.received_at as travellingto,  
+    workitems_unassigned.received_at as unassigned,  
+    workitems_working.received_at as working    
+from workitems
+left join projects on projects.dbt_scd_id = workitems.projects_sk
+left join workitems_accepted using (work_item_id)
+left join workitems_closed using (work_item_id)
+left join workitems_assigned using (work_item_id)
+left join workitems_cancelled using (work_item_id)
+left join workitems_discarded using (work_item_id)
+left join workitems_postworking using (work_item_id)
+left join workitems_preworking using (work_item_id)
+left join workitems_quickclose using (work_item_id)
+left join workitems_remoteclosed using (work_item_id)
+left join workitems_travellingfrom using (work_item_id)
+left join workitems_travellingto using (work_item_id) 
+left join workitems_unassigned using (work_item_id)
+left join workitems_working using (work_item_id)
+)
+select * from vw_workitem_stages 
