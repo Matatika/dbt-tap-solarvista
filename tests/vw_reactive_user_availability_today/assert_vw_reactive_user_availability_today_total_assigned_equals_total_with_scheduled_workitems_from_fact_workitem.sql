43636ffa0cf3e02d00c8aaf1c574900a9eac06e8
Aaron Phethean <aphethean@matatika.com>
2021-03-20 06:29:46 +0000

    Updated tests to use not exists.  Using not exists allows the tests to work if either side has a null result and seems more clear of its intention to assert the correct result

--- tests/vw_reactive_user_availability_today/assert_vw_reactive_user_availability_today_total_assigned_equals_total_with_scheduled_workitems_from_fact_workitem.sql
+++ tests/vw_reactive_user_availability_today/assert_vw_reactive_user_availability_today_total_assigned_equals_total_with_scheduled_workitems_from_fact_workitem.sql
@@ -1,13 +1,13 @@
 -- Assert that all assigned users in vw_reactive_user_availability_today have active, scheduled work items in fact_workitem,
 -- where the scheduled start_time <= now for today
 select
-    *
+    user_id
 from {{ ref('vw_reactive_user_availability_today')}}
 where current_availability = 'assigned'
-having count(*) = (select count(*)
+and not exists (select assigned_user_id
                     from {{ ref('fact_workitem')}}
                     where schedule_start_date = current_date
                     and schedule_start_time <= now()
-                    and assigned_user_id notnull
+                    and assigned_user_id = user_id
                     and current_stage not in ('Closed', 'Cancelled', 'RemoteClosed', 'Discarded', 'Rejected', 'Unassigned')
                     and template_display_name != 'Non-Productive Time')
