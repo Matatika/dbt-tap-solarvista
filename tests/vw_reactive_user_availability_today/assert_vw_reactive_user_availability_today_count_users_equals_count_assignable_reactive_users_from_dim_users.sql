55cea84ceac00922a1ecb6d590ab8657b8c5b9e2
Daniel Walker <danielpdwalker@gmail.com>
2021-03-22 15:43:46 +0000

    Updated fact_user_assignment to have a scheduled_to_time columns based off appointment end time or scheduled work duration. Updated vw_reactive_user_availability_today to show the job that will take the max active scheduled_to_time assignment and the time they are scheduled on that assignment untill. Added fact_user_assignment test to assert no reasons are missing in the table. Added vw_reactive_user_availability_today test to make sure number of users in the view is correct compared to dim_user. Refactored a few vw_reactive_user_availability_today tests.

---
+++ tests/vw_reactive_user_availability_today/assert_vw_reactive_user_availability_today_count_users_equals_count_assignable_reactive_users_from_dim_users.sql
@@ -0,0 +1,7 @@
+-- Assert vw_reactive_user_availability_today contains only reactive, assignable users
+select
+    count(user_id)
+from {{ ref('vw_reactive_user_availability_today')}}
+having count(user_id) != (select count(user_id) from {{ ref('dim_user')}}
+                                        where is_assignable = true
+                                        and is_reactive = true)
