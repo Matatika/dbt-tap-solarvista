159f80eec8b0694ae1661a9834ea8dbf90230eb1
Aaron Phethean <aphethean@matatika.com>
2021-05-20 12:23:41 +0100

    Added equipment type to project dimension

--- models/base/dim_project.sql
+++ models/base/dim_project.sql
@@ -1,9 +1,20 @@
 {{ config(materialized='table') }}
 
-with dim_project as (
+with projects as (
     select * from {{ ref('dim_project_snapshot') }} 
     where dbt_valid_to is null
     and status != 'Discarded'
+),
+equipments as (
+    select * from "{{var('schema')}}".equipment_stream
+),
+
+dim_project as (
+    select
+        projects.*
+        , equipments.category_title
+    from projects
+    left join equipments on equipments.asset_number = projects.assetnumber
 )
 
 select * from dim_project
