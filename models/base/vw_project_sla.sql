6facd118a9c69ef7d6a24db5c5695d810c734419
Daniel Walker <danielpdwalker@gmail.com>
2021-06-02 14:15:42 +0100

    Updated vw_project_sla to have firstfix_date column

--- models/base/vw_project_sla.sql
+++ models/base/vw_project_sla.sql
@@ -19,6 +19,9 @@ territories as (
 dates as (
     select * from {{ ref('dim_date') }}
 ),
+project_snapshot as (
+    select * from {{ ref('dim_project_snapshot')}}
+),
 project_workitem_count as (
     select distinct
         projects.reference as project_id,
@@ -46,7 +49,13 @@ project_reactivated as (
     and workitems.tags ? 'Reactivation'
     group by projects.reference
 ),
-
+project_firstfix_date as (
+    select
+        project_snapshot.reference as project_id,
+        min(project_snapshot.closedon) as firstfix_date
+    from project_snapshot
+    group by project_snapshot.reference
+),
 project_sla as (
     select
         projects.project_sk
@@ -61,6 +70,7 @@ project_sla as (
         , min(workitem_stages.remoteclosed_timestamp) as remoteclosed_timestamp
         , min(workitem_stages.cancelled_timestamp) as cancelled_timestamp
         , min(workitem_stages.preworking_timestamp) as preworking_timestamp
+        , min(project_firstfix_date.firstfix_date) as firstfix_date
 
         , (case
              -- Use PreWorking time as first response or closedon
@@ -93,6 +103,7 @@ project_sla as (
             on workitem_stages.work_item_id = workitems.work_item_id
         left join project_workitem_active on project_workitem_active.project_id = projects.reference
         left join project_reactivated on project_reactivated.project_id = projects.reference
+        left join project_firstfix_date on project_firstfix_date.project_id = projects.reference
     group by projects.project_sk
 ),
 
@@ -114,6 +125,7 @@ stats as (
         cancelled_timestamp,
         preworking_timestamp,
         firstresponse_date,
+        firstfix_date,
         finalfix_date,
         is_open,
         is_closed,
@@ -180,6 +192,7 @@ final as (
         stats.preworking_timestamp, -- deprecated, update vw_daily_projects to use dim project snapshot and remove this
         stats.firstresponse_date,
         stats.firstresponse_date as first_response, -- deprecated, update reports to remove this
+        stats.firstfix_date,
         stats.finalfix_date,
         stats.finalfix_date as final_fix, -- deprecated, update reports to remove this
         stats.is_open,
