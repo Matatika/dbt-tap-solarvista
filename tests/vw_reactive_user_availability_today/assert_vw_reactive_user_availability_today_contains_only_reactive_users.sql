d99f4b3ac580d92c4fa86cd45fa3a26c3c793c60
danielpdwalker <danielpdwalker@gmail.com>
2021-08-23 13:52:44 +0100

    Deleted vw_reactive_user_availability_today

--- tests/vw_reactive_user_availability_today/assert_vw_reactive_user_availability_today_contains_only_reactive_users.sql
+++
@@ -1,8 +0,0 @@
--- Assert vw_reactive_user_availability_today contains only reactive, assignable users
-select
-    user_id
-from {{ ref('vw_reactive_user_availability_today')}}
-where user_id not in (select user_id from {{ ref('dim_user')}}
-                        where is_assignable = true
-                        and is_reactive = true)
-                        
