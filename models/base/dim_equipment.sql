bb09878cb03141c0737e85a92b85706c186ea15e
danielpdwalker <danielpdwalker@gmail.com>
2021-08-24 11:36:41 +0100

    Created dim_equipment

---
+++ models/base/dim_equipment.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='table') }}
+
+with equipment as (
+    select * from "{{var('schema')}}".equipment_stream
+),
+dim_equipment as (
+    select
+        {{ dbt_utils.surrogate_key(['reference']) }} as equipment_sk,
+        equipment.reference, 
+        equipment.asset_number, 
+        equipment.category_id, 
+        equipment.category_title, 
+        equipment.description,
+        equipment.location
+    from equipment
+)
+select * from dim_equipment
