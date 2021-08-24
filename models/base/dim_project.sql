b0abe043221975b1d1548573fdae875a4b09cbcd
danielpdwalker <danielpdwalker@gmail.com>
2021-08-24 11:37:12 +0100

    Updated dim_project to join to equipment for equipment_sk

--- models/base/dim_project.sql
+++ models/base/dim_project.sql
@@ -5,14 +5,16 @@ with projects as (
     where dbt_valid_to is null
     and status != 'Discarded'
 ),
-equipments as (
-    select * from "{{var('schema')}}".equipment_stream
+dim_equipment as (
+    select * from {{ ref('dim_equipment') }}
 ),
 
 dim_project as (
     select
         projects.*
+        , dim_equipment.equipment_sk
     from projects
+    left join dim_equipment on dim_equipment.asset_number = projects.assetnumber 
 )
 
 select * from dim_project
