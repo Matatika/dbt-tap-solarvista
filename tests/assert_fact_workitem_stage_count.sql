d3aacdcb19680aba93f13c76aca7dcb9b66bead5
danielpdwalker <danielpdwalker@gmail.com>
2021-08-31 10:08:55 +0100

    Deleted CBES specific tests, Removed CBES filtering from tests

--- tests/assert_fact_workitem_stage_count.sql
+++ tests/assert_fact_workitem_stage_count.sql
@@ -1,6 +1,5 @@
 --Compare workitem count stage wise with total workitems - to check duplicate workitem numbers or 
 --null data rows which could be error records from source
---Current data count: 10722 (8440-closed + 250-Accepted/assigned + 2032-other statuses) 
 select 'Workitem stage count error' as workitemtotal from 
 (select distinct count(work_item_id) as workitemtotal from {{ ref('fact_workitem' )}}) workitemtotal,
 (select count(work_item_id) as asnd from {{ ref('fact_workitem' )}} where current_stage 
--- tests/assert_fact_workitem_unassigned_check.sql
+++
@@ -1,9 +0,0 @@
---To check workitems status match between current and history workitems fact table
---Test workitem id - 01accd16-8ac2-48a6-abe5-68af028f2c9b
---No rows in history when workitem is in unassigned status in fact table 
-select f1.work_item_id,fwh.stage_stage_type 
-from {{ ref('fact_workitem' )}} f1, {{ ref('fact_workitem_history')}} fwh
-where f1.current_stage = 'Unassigned'
-and fwh.stage_stage_type = 'Closed'
-and f1.work_item_id = fwh.work_item_id 
---and f1.work_item_id = '01accd16-8ac2-48a6-abe5-68af028f2c9b'
