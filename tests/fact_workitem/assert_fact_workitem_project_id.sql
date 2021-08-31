a0a5fea661b4c48ec0453b1ec39d001f0ee43926
danielpdwalker <danielpdwalker@gmail.com>
2021-08-31 09:59:49 +0100

    Removed CBES specific template requirement from fact_workitem test

--- tests/fact_workitem/assert_fact_workitem_project_id.sql
+++ tests/fact_workitem/assert_fact_workitem_project_id.sql
@@ -1,9 +1,6 @@
--- Given a work item with 'Work Order / Job' or 'Work Order / Maintenance' template type
--- Expect project_id to always be set (returning results is a test failure)
+-- Given a work item expect project_id to always be set (returning results is a test failure)
 select
 	work_item_id, fact_workitem.reference, created_on, current_stage, project_id, template_display_name, dim_project.status
 from {{ ref('fact_workitem' )}} fact_workitem
 	left join {{ ref('dim_project' )}} as dim_project on dim_project.project_sk = fact_workitem.project_sk
 where project_id is null
-and template_display_name in ('Work Order / Job', 'Work Order / Maintenance', 'Work Order / Verisae')
-and assigned_user_id is not null  -- found one dodgy work item that is a work order type, but no project, but no user assigned so don't worry
