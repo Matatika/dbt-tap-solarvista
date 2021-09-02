6097db7bbef27a9591a5e61b3028e37de06c4709
danielpdwalker <danielpdwalker@gmail.com>
2021-09-02 14:21:46 +0100

    Deleted assert_fact_workitem_project_id

--- tests/fact_workitem/assert_fact_workitem_project_id.sql
+++
@@ -1,6 +0,0 @@
--- Given a work item expect project_id to always be set (returning results is a test failure)
-select
-	work_item_id, fact_workitem.reference, created_on, current_stage, project_id, template_display_name, dim_project.status
-from {{ ref('fact_workitem' )}} fact_workitem
-	left join {{ ref('dim_project' )}} as dim_project on dim_project.project_sk = fact_workitem.project_sk
-where project_id is null
